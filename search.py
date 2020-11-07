24958a8cefe586a16dfa9477b34b2a36dc1473d5
Colin Spiridonov <spiridon@usc.edu>
2020-11-06 17:30:08 -0800

    Yay now you can specify preferences on who to playtest with

--- search.py
+++ search.py
@@ -1,8 +1,34 @@
-
+import copy
 
 def start_search(all_times, playtesters, preferred_matches):
 	# Get order of playtesters in a list
 	playtester_order = list(playtesters.keys())
+	# Try to assign preferred matches
+	valid = False
+	for cohort_member, pref_playtesters in preferred_matches.items():
+		print("Looking for a match for " + cohort_member)
+		found_match = False
+		for playtester in pref_playtesters:
+			if found_match:
+				break
+			print("  with " + playtester)
+			for time, slot in all_times.items():
+				if found_match:
+					break
+				print("    at " + time)
+				if cohort_member in all_times[time].keys():
+					if all_times[time][cohort_member] == "FREE":
+						# print("     Assigning to " + time)
+						all_times[time][cohort_member] = playtester
+						all_times_copy = copy.deepcopy(all_times)
+						valid = backtracking_search(all_times_copy, playtesters, playtester_order)
+						if valid:
+							found_match = True
+						else:
+							all_times[time][cohort_member] = "FREE"
+						break
+				else:
+					continue
 	# Start backtracking search
 	valid = backtracking_search(all_times, playtesters, playtester_order)
 	print("\n")
@@ -19,13 +45,16 @@ def backtracking_search(all_times, playtesters, playtester_order, p=0):
 	if(p >= len(playtester_order)):
 		return True
 
+	if p == 0:
+		print("backtracking_search()...")
+
 	current_playtester = playtester_order[p]
-	print("Current playtester = " + current_playtester + ", p = " + str(p))
+	# print("Current playtester = " + current_playtester + ", p = " + str(p))
 
 	# Iterate through all possible times
 	time_available = False
 	for time in playtesters[current_playtester]:
-		print("  Trying time " + time)
+		# print("  Trying time " + time)
 		valid_time = False
 
 		# If time is valid, assign all variables
@@ -36,19 +65,19 @@ def backtracking_search(all_times, playtesters, playtester_order, p=0):
 		
 		# If time was picked, recurse
 		if valid_time:
-			print("  Picked " + time)
+			# print("  Picked " + time)
 			valid = backtracking_search(all_times, playtesters, playtester_order, p + 1)
 			# If recursing results in blockages, pick another time
 			if not valid:
 				# Unassign all variables
-				print("Back to playtester = " + current_playtester + ", p = " + str(p))
+				# print("Back to playtester = " + current_playtester + ", p = " + str(p))
 				all_times[time][cohort_member] = "FREE"
 				continue
 			else:
 				return True
 	# If no time was found, return false
 	if not time_available:
-		print("  No time was found, backtracking...")
+		# print("  No time was found, backtracking...")
 		return False
 
 
